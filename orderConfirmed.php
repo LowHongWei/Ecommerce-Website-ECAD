62d36fc726bee8a88ad0e839b5944ef8f67eaeff
ThuYasWe <s10222373@connect.np.edu.sg>
2024-01-12 19:20:53 +0800

    feat: Added Paypal checkout feature

---
+++ orderConfirmed.php
@@ -0,0 +1,13 @@
+<?php 
+session_start(); // Detect the current session
+$pageName = "Order Confirmed";
+include("header.php"); // Include the Page Layout header
+
+if(isset($_SESSION["OrderID"])) {	
+	echo "<p>Checkout successful. Your order number is $_SESSION[OrderID]</p>";
+	echo "<p>Thank you for your purchase.&nbsp;&nbsp;";
+	echo '<a href="index.php">Continue shopping</a></p>';
+} 
+
+include("footer.php"); // Include the Page Layout footer
+?>
